7ad75dbde38337d6e57234280aa008e96dc6b4ad
Jonathan Barnell <jbarnell@seas.upenn.edu>
2021-08-13 10:50:50 -0500

    added new cache file (private), fixed bug causing col_func_map to not be loaded to memory

--- khel_wgs_sc2/workflow/workflow_obj.py
+++ khel_wgs_sc2/workflow/workflow_obj.py
@@ -15,11 +15,9 @@ class workflow_obj(ABC):
         dirname = os.path.dirname(os.path.abspath(__file__))
         folders = dirname.split("\\")
         top_pkg_folder = "\\".join(folders[0:-2])
-        path_to_cache = top_pkg_folder + "\\data\\cache.json"
+        path_to_static_cache = top_pkg_folder + "\\data\\static_cache.json"
 
-        full_cache = read_json(path_to_cache)
-        # with open(path_to_cache, 'r') as json_cache:
-        #     full_cache = json.load(json_cache)
+        full_static_cache = read_json(path_to_static_cache)
         
         # only select today's workflow, check that static data is present
         if wf > 0:
@@ -36,9 +34,9 @@ class workflow_obj(ABC):
             elif wf == -5:
                 workflow = 'refresh'
 
-        working_cache = full_cache[workflow]
-        gen_cache = full_cache['all_workflows']
-        for k, v in working_cache.items():
+        working_static_cache = full_static_cache[workflow]
+        gen_static_cache = full_static_cache['all_workflows']
+        for k, v in working_static_cache.items():
             if "query" in str(k):
                 if not hasattr(self, k):
                     setattr(self, k, "".join(v))
@@ -46,36 +44,48 @@ class workflow_obj(ABC):
                 if not hasattr(self, k):
                     setattr(self, k, v)
 
-        # search for dynamic data.  If empty, gather from user (first time use)
+        self.avg_depth_cutoff = gen_static_cache['avg_depth_cutoff']
+        self.percent_cvg_cutoff = gen_static_cache['percent_cvg_cutoff']
+        self.neg_percent_cvg_cutoff = gen_static_cache['neg_percent_cvg_cutoff']
+        self.col_func_map = gen_static_cache['col_func_map']
+
+        # search for private data.  If empty, gather from user (first time use)
+        path_to_private_cache = top_pkg_folder + "\\data\\private_cache.json"
+        full_private_cache = read_json(path_to_private_cache)
+        working_private_cache = full_private_cache[workflow]
+        gen_private_cache = full_private_cache['all_workflows']
+
         try:
+            need_sql=True
+            self.sql_user = gen_private_cache['sql_user']
+            self.sql_pass = gen_private_cache['sql_pass']
+            self.sql_server = gen_private_cache['sql_server']
+            self.sql_db = gen_private_cache['sql_db']
+            need_sql=False
             if wf == 1:
-                self.lims_conn = working_cache['lims_conn']
+                self.lims_conn = working_private_cache['lims_conn']
             if wf == -2:
-                self.path_to_template = working_cache['path_to_template']
-                self.gisaid_path = working_cache['gisaid_path']
-                self.folderpathbase = working_cache['folderpathbase']
+                self.path_to_template = working_private_cache['path_to_template']
+                self.gisaid_path = working_private_cache['gisaid_path']
+                self.folderpathbase = working_private_cache['folderpathbase']
             if wf == 7:
-                self.destination = working_cache['destination']
-                self.location = working_cache['location']
-                self.port = working_cache['port']
-                self.sftp_user = working_cache['sftp_user']
-                self.sftp_pwd = working_cache['sftp_pwd']
+                self.destination = working_private_cache['destination']
+                self.location = working_private_cache['location']
+                self.port = working_private_cache['port']
+                self.sftp_user = working_private_cache['sftp_user']
+                self.sftp_pwd = working_private_cache['sftp_pwd']
             if wf == -4:
-                self.folder_path_base =working_cache['folder_path']
-            self.sql_user = gen_cache['sql_user']
-            self.sql_pass = gen_cache['sql_pass']
-            self.sql_server = gen_cache['sql_server']
-            self.sql_db = gen_cache['sql_db']
-            self.avg_depth_cutoff = gen_cache['avg_depth_cutoff']
-            self.percent_cvg_cutoff = gen_cache['percent_cvg_cutoff']
-            self.neg_percent_cvg_cutoff = gen_cache['neg_percent_cvg_cutoff']
-            self.col_func_map = gen_cache['col_func_map']
+                self.folder_path_base = working_private_cache['folder_path_base']
+            if wf == 6:
+                self.lab = working_private_cache['lab']
+                self.p_lab = working_private_cache['p_lab']
+
         except KeyError:
             print("Looks like this is your first time using the script!")
             print("\nPlease fill out the following questions (we'll store \
 results on-device so you won't have to enter them again).")
             if wf == 1:
-                self.lims_conn = input("Please enter the string for the LIMS connection \
+                self.lims_conn = input("\nPlease enter the string for the LIMS connection \
 (ie <user>/<password>@<db_tablename>\n-->")
             if wf == -2:
                 print("Please enter the path to the template workbook")
@@ -84,41 +94,51 @@ results on-device so you won't have to enter them again).")
                 self.gisaid_path = get_path()
                 self.folderpathbase = "\\".join(self.path_to_template.split("\\")[:-1])
             if wf == 7:
-                self.destination = input("Type the relative path of the destination folder")
-                self.location = input("Type the address of the final location of sftp transfer")
-                self.port = input("Type the port to be used in transfer (typically 22)")
-                self.sftp_user = input("Type the username for sftp access")
-                self.sftp_pwd = input("Type the password for sftp access")
+                self.destination = input("\nType the relative path of the destination folder\n-->")
+                self.location = input("\nType the address of the final location of sftp transfer\n-->")
+                self.port = input("\nType the port to be used in transfer (typically 22)\n-->")
+                self.sftp_user = input("\nType the username for sftp access\n-->")
+                self.sftp_pwd = input("\nType the password for sftp access\n-->")
             if wf == -4:
-                print("Please select the path to the folder where you would like the \
+                print("\nPlease select the path to the folder where you would like the \
                     queries to be stored.")
                 self.folder_path_base = get_path()
-            self.sql_user = input("Please enter the username for the sql database:\n-->")
-            self.sql_pass = input("Please enter the password for the sql database:\n-->")
-            self.sql_server = input("Please enter the server name for the sql database:\n-->")
-            self.sql_db = input("Please enter the name for the sql database:\n-->")
+            if wf == 6:
+                self.lab = input("\nType the name of the lab submitting the report\n-->")
+                self.p_lab = input("\nType the name of the lab performing the tests to appear on this report\n-->")
+
+            if need_sql:
+                self.sql_user = input("\nPlease enter the username for the sql database:\n-->")
+                self.sql_pass = input("\nPlease enter the password for the sql database:\n-->")
+                self.sql_server = input("\nPlease enter the server name for the sql database:\n-->")
+                self.sql_db = input("\nPlease enter the name for the sql database:\n-->")
 
             
             print("\nFinished! If you need to change these values in the \
-future for any reason, modify the cache file: daily_workflow/data/cache.json")
+future for any reason, modify the cache file: daily_workflow/data/private_cache.json")
             if wf == 1:
-                full_cache[workflow]['lims_conn'] = self.lims_conn
+                full_private_cache[workflow]['lims_conn'] = self.lims_conn
             if wf == -2:
-                full_cache[workflow]['path_to_template'] = self.path_to_template
-                full_cache[workflow]['gisaid_path'] = self.gisaid_path
-                full_cache[workflow]['folderpathbase'] = self.folderpathbase
+                full_private_cache[workflow]['path_to_template'] = self.path_to_template
+                full_private_cache[workflow]['gisaid_path'] = self.gisaid_path
+                full_private_cache[workflow]['folderpathbase'] = self.folderpathbase
             if wf == 7:
-                full_cache[workflow]['destination'] = self.destination
-                full_cache[workflow]['location'] = self.location
-                full_cache[workflow]['port'] = self.port
-                full_cache[workflow]['sftp_user'] = self.sftp_user
-                full_cache[workflow]['sftp_pwd'] = self.sftp_pwd
-            full_cache["all_workflows"]['sql_user'] = self.sql_user
-            full_cache["all_workflows"]['sql_pass'] = self.sql_pass
-            full_cache["all_workflows"]['sql_server'] = self.sql_server
-            full_cache["all_workflows"]['sql_db'] = self.sql_db
+                full_private_cache[workflow]['destination'] = self.destination
+                full_private_cache[workflow]['location'] = self.location
+                full_private_cache[workflow]['port'] = self.port
+                full_private_cache[workflow]['sftp_user'] = self.sftp_user
+                full_private_cache[workflow]['sftp_pwd'] = self.sftp_pwd
+            if wf == -4:
+                full_private_cache[workflow]['folder_path_base'] = self.folder_path_base
+            if wf == 6:
+                full_private_cache[workflow]['lab'] = self.lab
+                full_private_cache[workflow]['p_lab'] = self.p_lab
+            full_private_cache["all_workflows"]['sql_user'] = self.sql_user
+            full_private_cache["all_workflows"]['sql_pass'] = self.sql_pass
+            full_private_cache["all_workflows"]['sql_server'] = self.sql_server
+            full_private_cache["all_workflows"]['sql_db'] = self.sql_db
             print("\nStoring data for future use...")
-            res = write_json(path_to_cache, full_cache)
+            res = write_json(path_to_private_cache, full_private_cache)
             if res != 0:
                 raise ValueError("workflow_obj write to json failed!")
             # with open(path_to_cache, mode='w') as cache_file:
