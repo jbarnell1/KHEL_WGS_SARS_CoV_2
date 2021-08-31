dd7581eb0d6e6deeafc7c812604c9212894439d4
Jonathan Barnell <jbarnell@seas.upenn.edu>
2021-08-31 15:10:08 -0500

    updating functionality for nc/po cli through ssh

--- khel_wgs_sc2/workflow/WF_5_parse_pangolin/WF_5_parse_pangolin.py
+++ khel_wgs_sc2/workflow/WF_5_parse_pangolin/WF_5_parse_pangolin.py
@@ -1,15 +1,24 @@
 import time
 from workflow.WF_5_parse_pangolin.WF_5_helpers import WorkflowObj5
 
-def run_script_5():
+def run_script_5(compiled_fasta_path):
     print("\n================================\nRun Data Import Script\n================================\n\n")
 
     # import relevant data from json file
     data_obj = WorkflowObj5()
     data_obj.get_json()
 
-    # open demo path --> pandas dataframe
-    data_obj.get_pango_dfs()
+    if compiled_fasta_path:
+        target_folders = compiled_fasta_path.split("/")
+        target_folder = "/".join(target_folders[:-1])
+        data_obj.send_fasta(compiled_fasta_path)
+        data_obj.run_pangolin()
+        data_obj.receive_pangolin_df(target_folder)
+        data_obj.get_pango_dfs(po_path=target_folder + "/results.csv")
+
+    else:
+        # open demo path --> pandas dataframe
+        data_obj.get_pango_dfs()
 
     # push results to database
     data_obj.database_push()
